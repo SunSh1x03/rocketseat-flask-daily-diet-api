fdd152d2cbbffca68232132c1107fafe77fd1a5b
Filipe Penido <filipenido@gmail.com>
2026-02-25 19:12:04 -0300

    feat(tasks): add GET endpoints for listing and retrieving tasks by id

--- app.py
+++ app.py
@@ -2,17 +2,38 @@ from flask import Flask, request, jsonify
 from models.task import Task
 app = Flask(__name__)
 
-task= []
+tasks= []
 task_id_control = 1
 
-@app.route('/task', methods=['POST'])
+@app.route('/tasks', methods=['POST'])
 def create_task():
     global task_id_control
     data = request.get_json()
     new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
     task_id_control += 1 
-    task.append(new_task)
+    tasks.append(new_task)
+    print(tasks)
     return jsonify({"message": "Nova refeição adicionada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list =[task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possível encontrar a refeição."}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
